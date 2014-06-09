95ce8cd7b837acbbb2447f3dd4cff9460dfa74ff
Giedrius Ramasauskas <gramasauskas@gmail.com>
2014-06-09 20:38:47 +0300

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   def show
   	@user = User.find(params[:id])
   end
@@ -18,12 +21,53 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+  
+  def edit
+  end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_path
+  end
+  
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, 
-    									             :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+    									               :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      #notice: , is same as flash[:notice], its a hash of redirect_to
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
 end
